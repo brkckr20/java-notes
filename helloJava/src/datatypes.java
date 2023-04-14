beeabee51c3b7efe9f76cfe1e3df0841ba18af54
brkckr20 <burak2043@gmail.com>
2023-04-14 12:12:34 +0300

    hello java eklendi

---
+++ helloJava/src/datatypes.java
@@ -0,0 +1,13 @@
+
+public class datatypes {
+    public static void main(String[] args) {
+        int sayi = 12;
+        byte sayi1 = 127; // max 127 alır ve min -128
+        //sayi = "0"; // hata verir;
+        
+        
+        // int sayi2 = 10.0; // hata verir
+        char x = 'a'; // char ise tek tırnak olmalıdır.
+        
+    }
+}
---
+++ helloJava/src/variables.java
@@ -0,0 +1,10 @@
+
+public class variables {
+
+    public static void main(String[] args) {
+        int ogrenciSayisi = 10;
+        String info = "Öğrenci Sayısı :";
+        System.out.println(info + ogrenciSayisi);
+        System.out.println("Öğrenci Sayısı : " + 6);
+    }
+}
